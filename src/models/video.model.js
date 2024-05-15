cab31c91261d93dbfa685c0e9d1bed0eeaac47bb
Vaibhavkumar Patel <vaibhavpatel162002@gmail.com>
2024-05-15 14:48:20 +0530

    added user and video models.

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudnary url
+      requiured: true,
+    },
+    thumbnail: {
+      type: String, // cloudnary url
+      requiured: true,
+    },
+    title: {
+      type: String,
+      requiured: true,
+    },
+    description: {
+      type: String, // cloudnary url
+      requiured: true,
+      s,
+    },
+    duration: {
+      type: Number, // from cloudnary
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
